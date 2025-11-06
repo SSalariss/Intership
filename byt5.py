f97900aea4c25ab49381e5a1f22ff02ee1b4cee6
Samuele Salaris <salaris.2083011@studenti.uniroma1.it>
2025-11-06 18:43:16 +0100

    restore

--- byt5.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoModel, AutoTokenizer
-
-model = AutoModel.from_pretrained("google/byt5-small")
-tokenizer = AutoTokenizer.from_pretrained("google/byt5-small")
-
-model.save_pretrained("./byt5_models/byt5_small")
-tokenizer.save_pretrained("./byt5_models/byt5_small")
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from tirocinio!")
-
-
-if __name__ == "__main__":
-    main()
