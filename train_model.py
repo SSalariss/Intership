7fb381592f7a13cddea8a7fafd35dd3458596ee0
Samuele Salaris <salaris.2083011@studenti.uniroma1.it>
2025-11-11 18:21:25 +0100

    modify config

--- train_model.py
+++ train_model.py
@@ -21,10 +21,10 @@ CONFIG = {
     'dataset_dir': './dataset',
     'model_name': 'google/byt5-small',
     'max_length': 1024,
-    'batch_size': 16,
-    'learning_rate': 1e-5,
+    'batch_size': 32,
+    'learning_rate': 3e-5,
     'num_epochs': 20,
-    'device': 'cuda' if torch.cuda.is_available() else 'cpu',
+    'device': DEVICE,
     'seed': 42,
     'save_dir': './models',
     'debug_mode': True,      # True = usa subset, False = dataset completo
